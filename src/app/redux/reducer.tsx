8b5c5a2e96ccc3a5025a7c2bcb092efb34da4af7
LoiBH <loi.buihuu@vti.com.vn>
2023-01-12 15:46:36 +0700

    create redux core

---
+++ src/app/redux/reducer.tsx
@@ -0,0 +1,18 @@
+const initState = {
+  filters: {
+    search: "",
+    status: "All",
+    priority: [],
+  },
+  todoList: [{}],
+};
+
+const rootReducer = (state = initState, action) => {
+  switch (actions.type) {
+    case "add":
+      break;
+
+    default:
+      break;
+  }
+};
--- src/app/redux/stores.ts
+++ src/app/redux/stores.ts
@@ -1,11 +1,18 @@
-import { configureStore } from "@reduxjs/toolkit";
-import slices from "./slices";
+// import { configureStore } from "@reduxjs/toolkit";
+// import slices from "./slices";
 
-const rootReducer = {
-  slice: slices,
-};
+// const rootReducer = {
+//   slice: slices,
+// };
 
-const store = configureStore({
-  reducer: rootReducer,
-});
+// const store = configureStore({
+//   reducer: rootReducer,
+// });
+// export default store; --> toolkit
+
+// core
+import { createStore } from "redux";
+
+const store = createStore(rootReducer);
+
 export default store;
